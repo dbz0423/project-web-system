d5c27d43670dfef5447b0c9f4d7b93dea9219bc4
dbz0423 <2569876934@qq.com>
2024-11-14 13:24:21 +0800

    feat: 菜单管理模块接口开发

---
+++ base-web/src/main/java/top/zhu/web/sys_menu/mapper/SysMenuMapper.java
@@ -0,0 +1,7 @@
+package top.zhu.web.sys_menu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import top.zhu.web.sys_menu.entity.SysMenu;
+
+public interface SysMenuMapper extends BaseMapper<SysMenu> {
+}
